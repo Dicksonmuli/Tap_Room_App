7733ef66efb0e967bbfd2f8d0597e7b80286f608
dicksonmuli <dicksonmully6@gmail.com>
2017-03-23 09:42:28 +0300

    main component-  added selectors to the appcomponent  and taking functions and variables from child components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,37 @@
+//imports
 import { Component } from '@angular/core';
 import { Bar } from './bar.model';
 
+//componet decorator which runs on my-app selector on index
 @Component({
   selector: 'my-app',
   template: `
-  
+  <div class="container" id = "first" >
+
+<div class="col-xs-4" >
+<h1>{{title}}</h1>
+<keg-list
+    [childKegList]="masterKegList"
+    (clickSender)="showDetails($event)"
+   ></keg-list>
+</div>
+<div class="col-xs-4" >
+<edit-keg
+[selectedChildKeg]="selectedKeg"
+(doneClickedSender)="edited()"
+></edit-keg>
+</div>
+<div class="col-xs-4" >
+<new-keg
+(newKegSender)="newKeg($event)"
+></new-keg>
+</div>
+</div>
   `
 
 })
 
+//main class
 export class AppComponent{
 
 }
--- app/bar.model.ts
+++ app/bar.model.ts
@@ -1,4 +1,4 @@
-
+//Bar class containing a constructor with all the properties
 export class Bar{
   constructor( public id: number, public name: string, public description: string, public price: number, public done: boolean)
   {};

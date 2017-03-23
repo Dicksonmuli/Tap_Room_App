b0aaed71c2b362cb39360d664f38ac59b4cfa00b
dicksonmuli <dicksonmully6@gmail.com>
2017-03-23 10:01:04 +0300

    edit file An edit file that the user can edit a beer on stock.

---
+++ app/edit-keg.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Bar } from './bar.model';
+
+@Component({
+  selector: 'edit-keg',
+  template: `
+    <div *ngIf="selectedChildKeg" class="">
+      <h1>Enter Keg Info</h1>
+      <div>
+        <label> Keg ID:</label>
+        <input [(ngModel)]="selectedChildKeg.id">
+      </div>
+      <div>
+        <label>Name:</label>
+        <input [(ngModel)]="selectedChildKeg.name">
+      </div>
+      <div>
+        <label>Description:</label>
+        <input [(ngModel)]="selectedChildKeg.description">
+      </div>
+      <div>
+        <label>Price:</label>
+        <input [(ngModel)]="selectedChildKeg.price">
+        <button (click)="doneClicked()">Save</button>
+      </div>
+    </div>
+  `
+})
+
+export class EditKegComponent {
+  
+
+}

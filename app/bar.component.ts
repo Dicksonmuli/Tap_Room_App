83e75fcfc2a6e8aed818544b75e39d5402adc3a6
dicksonmuli <dicksonmully6@gmail.com>
2017-03-23 09:55:56 +0300

    bar component edited- templete and function to enable the user to check out on checkbox

--- app/bar.component.ts
+++ app/bar.component.ts
@@ -4,11 +4,21 @@ import { Bar } from './bar.model';
 @Component ({
    selector: 'keg-display',
    template: `
-
-
+   <div class="row">
+    <div class="col-md-8">
+    <label>{{ keg.description }}</label>
+    </div>
+    <div class="col-md-4">
+      <input *ngIf="keg.done === true" type="checkbox" checked (click)="toggleDone(false)"/>
+        <input *ngIf="keg.done === false" type="checkbox" (click)="toggleDone(true)"/>
+    </div>
+    </div>
    `
 
 })
 export class BarComponent{
-  
+  @Input() keg: Bar;
+   toggleDone(setCompleteness: boolean) {
+     this.keg.done = setCompleteness;
+   }
   }

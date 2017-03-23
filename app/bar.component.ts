912f1680c61c5ff338e48d643f644840007075bf
dicksonmuli <dicksonmully6@gmail.com>
2017-03-23 09:53:33 +0300

    checkbox file- creating a checkbox file for the user to be able to check out if the keg is out of stock

---
+++ app/bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Bar } from './bar.model';
+
+@Component ({
+   selector: 'keg-display',
+   template: `
+
+
+   `
+
+})
+export class BarComponent{
+  
+  }
